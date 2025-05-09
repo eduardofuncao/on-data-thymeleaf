658eeea8ca1692ff561d1bc1d2bc487fe7dfe2b6
Eduardo Função <eduardofuncao@hotmail.com>
2025-05-09 00:11:15 -0300

    update layout of homepage and navbar to account for user role visibility of elements

---
+++ src/main/java/br/com/fiap/on_data_thymeleaf/controller/GlobalControllerAdvice.java
@@ -0,0 +1,28 @@
+package br.com.fiap.on_data_thymeleaf.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class GlobalControllerAdvice {
+
+    @ModelAttribute("isAdmin")
+    public boolean isAdmin() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null) {
+            return false;
+        }
+
+        for (GrantedAuthority authority : authentication.getAuthorities()) {
+            if ("ROLE_ADMIN".equals(authority.getAuthority())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
